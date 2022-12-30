3b6e7846558b599e13b23082bfa5c2584ff79993
Sanjay <sanjaykumarme40@gmail.com>
2022-12-31 00:36:41 +0530

    table-update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
+import React from 'react'
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import './App.css';
 // import Pop3 from './Components/Pages/Pop3';
-import Last from './Components/Pages/Last';
-// import Second from './Components/Pages/Second';
+// import Last from './Components/Pages/Last';
+import Second from './Components/Pages/Second';
 
 function App() {
   return (
     <div className="App">
-      <Last/>
-      {/* <Second/> */}
+      {/* <Last/> */}
+      <Second/>
       {/* <Pop3/> */}
     </div>
   );
