466692cbf2c801c01404a45a0cc9fde3024e5ed2
Sanjay <sanjaykumarme40@gmail.com>
2023-01-02 23:57:09 +0530

    work

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react'
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import './App.css';
 // import Pop3 from './Components/Pages/Pop3';
- import Last from './Components/Pages/Last';
+//  import Last from './Components/Pages/Last';
 // import Second from './Components/Pages/Second';
+import Work from './Components/Pages/Work';
 
 function App() {
   return (
     <div className="App">
-      <Last/>
+      {/* <Last/> */}
       {/* <Second/> */}
       {/* <Pop3/> */}
+      <Work/>
     </div>
   );
 }
